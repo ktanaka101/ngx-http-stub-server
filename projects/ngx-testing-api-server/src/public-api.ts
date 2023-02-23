a9a715477d16a9e001f8edfba6fc11518cacc757
ktanaka101 <kentanaka101@gmail.com>
2023-02-23 14:41:32 +0900

    wip

--- projects/ngx-testing-api-server/src/public-api.ts
+++ projects/ngx-testing-api-server/src/public-api.ts
@@ -1,7 +1,7 @@
-/*
- * Public API Surface of ngx-testing-api-server
- */
-
-export * from './lib/ngx-testing-api-server.service';
-export * from './lib/ngx-testing-api-server.component';
-export * from './lib/ngx-testing-api-server.module';
+export { HttpClientTestingBackendController } from './lib/ngx-testing-api-server';
+export {
+  ApiHandler,
+  handlerBuilder,
+  responseBuilder,
+  setupTestingServer,
+} from './lib/handler';
